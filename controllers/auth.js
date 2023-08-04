511686c00c24d99d42a380d2c6d99b47e71ba51a
cheesechoux <gsoosuu@gmail.com>
2023-08-04 22:50:45 +0900

    feat: login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const ConflictException = require('../exceptions/conflict-exception');
 
@@ -43,5 +44,39 @@ exports.signup = async (req, res) => {
         } else {
             res.status(500).json({ error: error.message });
         }
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // 이메일과 비밀번호 유효성 검사
+        if (!email || !password) {
+            throw new Error('이메일과 비밀번호를 입력해주세요.');
+        }
+        const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!EMAIL_REGEX.test(email)) {
+            throw new Error('올바른 이메일 형식이 아닙니다.');
+        }
+
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            throw new Error('해당 이메일의 사용자를 찾을 수 없습니다.');
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            throw new Error('비밀번호가 일치하지 않습니다.');
+        } else if (password.length < MIN_PASSWORD_LENGTH) {
+            throw new Error('비밀번호는 8자 이상이어야 합니다.');
+        }
+
+        const JWT_SECRET_KEY = 'your-secret-key';
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET_KEY, { expiresIn: '1h' });
+
+        res.status(200).json({ user, token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
     }
 };
